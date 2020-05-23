244c252189314b0c1f7042d343588efbcf216299
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-05-24 00:46:29 +0200

    refactor : now a pure utility class

--- plugins/fr.lip6.puck.core/src/fr/lip6/puck/core/GraphBuilder.java
+++ plugins/fr.lip6.puck.core/src/fr/lip6/puck/core/GraphBuilder.java
@@ -17,155 +17,26 @@ import org.eclipse.jdt.core.dom.PackageDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
-public class GraphBuilder extends ASTVisitor {
-	private PuckGraph graph;
-	private Stack<Integer> currentOwner = new Stack<>();
 
-	public GraphBuilder(DependencyNodes nodes) {
-		this.graph = new PuckGraph(nodes);
-	}
-
-	/**
-	 * The actual visit a "Name" case, resolve the node it points to, and add a dependency to it.
-	 * @param node every node in the full AST gets this method invoked on it; the other visit methods are more selective, this is a catch-all.
-	 */
-	@Override
-	public void preVisit(ASTNode node) {
-		if (node instanceof Name) {
-			Name name = (Name) node;
-			IBinding b = name.resolveBinding();
-			addDependency(b,node);
-		}
-		super.preVisit(node);
-	}	
+/**
+ * Traverses a set of input Java files to build the underlying containment and usage graphs.
+ * @author Yann
+ *
+ */
+public class GraphBuilder  {
 
 	/**
-	 * Add a dependency from top of curentOwner stack (if any) to the node referred to in the binding (if any).
-	 * @param tb A @see {@link IBinding} resolved name that might point to a node of the graph.
-	 * @param node 
+	 * Visits the parsed AST of these compilation units to first collect a set of nodes,
+	 * then add all usage and containment edges to this graph.
+	 * @param parsedCu A list of parsed Java DOM representations of sources.
+	 * @return a graph containing the analysis results.
 	 */
-	private void addDependency(IBinding tb, ASTNode node) {
-		if (!currentOwner.isEmpty()) {
-			int indexSrc = currentOwner.peek();
-			int indexDst = graph.getNodes().findIndex(tb);
-			if (indexDst != indexSrc && indexDst >= 0 && indexSrc >= 0) {
-				graph.getUseGraph().addEdge(indexDst, indexSrc, node);
-			}
-		}
-	}	
-	
-	/**
-	 * Owner becomes the current type declaration.
-	 */
-	@Override
-	public boolean visit(TypeDeclaration node) {
-		ITypeBinding type = node.resolveBinding();
-		int cur = graph.getNodes().findIndex(type);
-		if (!currentOwner.isEmpty()) {
-			graph.getComposeGraph().addEdge(cur, currentOwner.peek(), node);
-		}
-		int parent = graph.findIndex(type.getPackage());
-		graph.getComposeGraph().addEdge(cur, parent, node);		
-		currentOwner.push(cur);		
-		return true;
-	}
-
-	@Override
-	public void endVisit(TypeDeclaration node) {
-		currentOwner.pop();		
-	}
-
-	/**
-	 * Current owner becomes the method declaration.
-	 */
-	@Override
-	public boolean visit(MethodDeclaration node) {
-		int cur = graph.getNodes().findIndex(node.resolveBinding());
-		if (!currentOwner.isEmpty()) {
-			graph.getComposeGraph().addEdge(cur, currentOwner.peek(), node);
-		}
-		currentOwner.push(cur);
-		return true;
-	}
-
-	@Override
-	public void endVisit(MethodDeclaration node) {
-		currentOwner.pop(); 
-	}
-
-	/**
-	 * Current owner becomes the field declaration.
-	 */
-	@Override
-	public boolean visit(VariableDeclarationFragment node) {
-		if (node.getParent() instanceof FieldDeclaration) {
-			int cur = graph.getNodes().findIndex(node.resolveBinding());
-			if (!currentOwner.isEmpty()) {
-				graph.getComposeGraph().addEdge(cur, currentOwner.peek(), node);
-			}
-			currentOwner.push(cur);
-		}
-		return true;
-	}
-
-	@Override
-	public void endVisit(VariableDeclarationFragment node) {
-		if (node.getParent() instanceof FieldDeclaration) {
-			currentOwner.pop();
-		} 
-	}
-
-	/**
-	 * Accessor for user : the result of traversing the compilation units.
-	 * @return
-	 */
-	public PuckGraph getGraph() {
-		return graph;
-	}
-	
-//	/**
-//	 * Accessor for user : the result of traversing the compilation units.
-//	 * @return
-//	 */
-//	public MatrixCol getComposeGraph() {
-//		if (composeGraph == null) {
-//			for (ITypeBinding type : types) {
-//				IPackageBinding pkg = type.getPackage();
-//				if (!packages.contains(pkg)) {
-//					packages.add(pkg);
-//					nodes.add(pkg);
-//				}
-//			}
-//			composeGraph = new MatrixCol(nodes.size(), nodes.size());
-//			// containment edges
-//			for (IMethodBinding meth: methods) {
-//				int elt = findIndex(nodes, meth);
-//				int parent = findIndex(nodes, meth.getDeclaringClass());
-//				if (elt >= 0 && parent >= 0) {
-//					composeGraph.set(elt, parent, 1);
-//				}
-//			}
-//			for (IVariableBinding meth: attributes) {
-//				int elt = findIndex(nodes, meth);
-//				int parent = findIndex(nodes, meth.getDeclaringClass());
-//				if (elt >= 0 && parent >= 0) {
-//					composeGraph.set(elt, parent, 1);
-//				}
-//			}
-//			for (ITypeBinding type : types) {
-//				int elt = findIndex(nodes, type);
-//				int parent = findIndex(nodes, type.getPackage());
-//				if (elt >= 0 && parent >= 0) {
-//					composeGraph.set(elt, parent, 1);
-//				}
-//			}
-//		}
-//		return composeGraph;
-//	}
-
 	public static PuckGraph collectGraph(List<CompilationUnit> parsedCu) {
+		// The nodes we will collect in a first pass.
 		DependencyNodes nodes = new DependencyNodes();		
-		
+
+		// First pass with a visitor.
+		// This visitor is not stateful so we used an anonymous class.
 		// actual traversal to find all relevant nodes we will consider within the scope of our graph. 
 		for (CompilationUnit unit : parsedCu) {
 
@@ -200,18 +71,136 @@ public class GraphBuilder extends ASTVisitor {
 				}				
 			});
 		}
-		
+
 		System.out.println("Found " + nodes.size() + " nodes : " + nodes);
-		
+
+		// Step 2 : we traverse the AST again, but this time we add edges to the picture
+		// if either source or destination for a potential edge is not in the set of nodes
+		// that we have collected above, the edge is discarded.
+
+		// Let's initialize the graph with the context nodes we have collected.
+		PuckGraph graph = new PuckGraph(nodes);
+
 		// Now the graph builder; because it is stateful (it keeps track of which node is current owner) it is implemented as a separate Visitor class.
-		// Let's give it the context we have collected.
-		GraphBuilder gb = new GraphBuilder(nodes);
-		
-		// now build the graph dependency links.
+		EdgeCollector edgeCollector = new EdgeCollector(graph);
+
+		// now build the graph dependency links using our visitor.
 		for (CompilationUnit unit : parsedCu) {
-			unit.accept(gb);
+			unit.accept(edgeCollector);
 		}
-		return gb.graph;
+
+		return graph;
+	}
+
+	/**
+	 * A visitor for AST that collects edges for any usage or containment 
+	 * where both nodes belong to the graph.
+	 *
+	 */
+	private static class EdgeCollector extends ASTVisitor {
+		private PuckGraph graph;
+
+		private Stack<Integer> currentOwner = new Stack<>();
+
+		public EdgeCollector(PuckGraph graph) {
+			this.graph = graph;
+		}
+
+
+		/**
+		 * The actual visit a "Name" case, resolve the node it points to, and add a dependency to it.
+		 * @param node every node in the full AST gets this method invoked on it; the other visit methods are more selective, this is a catch-all.
+		 */
+		@Override
+		public void preVisit(ASTNode node) {
+			if (node instanceof Name) {
+				Name name = (Name) node;
+				IBinding b = name.resolveBinding();
+				addDependency(b,node);
+			}
+			super.preVisit(node);
+		}	
+
+		/**
+		 * Add a dependency from top of curentOwner stack (if any) to the node referred to in the binding (if any).
+		 * @param tb A @see {@link IBinding} resolved name that might point to a node of the graph.
+		 * @param node 
+		 */
+		private void addDependency(IBinding tb, ASTNode node) {
+			if (!currentOwner.isEmpty()) {
+				int indexSrc = currentOwner.peek();
+				int indexDst = graph.getNodes().findIndex(tb);
+				if (indexDst != indexSrc && indexDst >= 0 && indexSrc >= 0) {
+					graph.getUseGraph().addEdge(indexDst, indexSrc, node);
+				}
+			}
+		}	
+
+		/**
+		 * Owner becomes the current type declaration.
+		 */
+		@Override
+		public boolean visit(TypeDeclaration node) {
+			ITypeBinding type = node.resolveBinding();
+			// is there a containment edge ?
+			int cur = graph.getNodes().findIndex(type);
+			if (!currentOwner.isEmpty()) {
+				graph.getComposeGraph().addEdge(cur, currentOwner.peek(), node);
+			}
+			// also add that this is contained in its package
+			// so a direct containment link from package to any type definition.
+			int parent = graph.findIndex(type.getPackage());
+			graph.getComposeGraph().addEdge(cur, parent, node);		
+			// we are new owner
+			currentOwner.push(cur);		
+			return true;
+		}
+
+		@Override
+		public void endVisit(TypeDeclaration node) {
+			currentOwner.pop();		
+		}
+
+		/**
+		 * Current owner becomes the method declaration.
+		 */
+		@Override
+		public boolean visit(MethodDeclaration node) {
+			int cur = graph.getNodes().findIndex(node.resolveBinding());
+			if (!currentOwner.isEmpty()) {
+				graph.getComposeGraph().addEdge(cur, currentOwner.peek(), node);
+			}
+			currentOwner.push(cur);
+			return true;
+		}
+
+		@Override
+		public void endVisit(MethodDeclaration node) {
+			currentOwner.pop(); 
+		}
+
+		/**
+		 * Current owner becomes the field declaration.
+		 */
+		@Override
+		public boolean visit(VariableDeclarationFragment node) {
+			if (node.getParent() instanceof FieldDeclaration) {
+				int cur = graph.getNodes().findIndex(node.resolveBinding());
+				if (!currentOwner.isEmpty()) {
+					graph.getComposeGraph().addEdge(cur, currentOwner.peek(), node);
+				}
+				currentOwner.push(cur);
+			}
+			return true;
+		}
+
+		@Override
+		public void endVisit(VariableDeclarationFragment node) {
+			if (node.getParent() instanceof FieldDeclaration) {
+				currentOwner.pop();
+			} 
+		}
+
 	}
 
 }
