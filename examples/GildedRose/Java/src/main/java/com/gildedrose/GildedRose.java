3e8cfb0718fd2148b4ea61b5504a2909f9e22073
Mikal Ziane <Mikal.Ziane@gmail.com>
2020-11-26 12:55:23 +0100

    replaced conditional with polymorphism

--- examples/GildedRose/Java/src/main/java/com/gildedrose/GildedRose.java
+++ examples/GildedRose/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-    private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 	Item[] items;
 
     public GildedRose(Item[] items) {
@@ -16,53 +15,6 @@ class GildedRose {
     }
 
 	private void updateQuality(Item item) {
-		if (!item.name.equals("Aged Brie")
-		        && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-		    if (item.quality > 0) {
-		        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-		            item.quality = item.quality - 1;
-		        }
-		    }
-		} else {
-		    if (item.quality < 50) {
-		        item.quality = item.quality + 1;
-
-		        if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-		            if (item.sellIn < 11) {
-		                if (item.quality < 50) {
-		                    item.quality = item.quality + 1;
-		                }
-		            }
-
-		            if (item.sellIn < 6) {
-		                if (item.quality < 50) {
-		                    item.quality = item.quality + 1;
-		                }
-		            }
-		        }
-		    }
-		}
-
-		if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-		    item.sellIn = item.sellIn - 1;
-		}
-
-		if (item.sellIn < 0) {
-		    if (!item.name.equals("Aged Brie")) {
-		        if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-		            if (item.quality > 0) {
-		                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-		                    item.quality = item.quality - 1;
-		                }
-		            }
-		        } else {
-		            item.quality = item.quality - item.quality;
-		        }
-		    } else {
-		        if (item.quality < 50) {
-		            item.quality = item.quality + 1;
-		        }
-		    }
-		}
+		ItemUpdaterFactory.createUpdater(item).updateQuality(item);
 	}
 }
