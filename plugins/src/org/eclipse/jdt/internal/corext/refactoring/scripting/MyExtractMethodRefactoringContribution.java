4f0c2e820600d33a0d77592df7db08c3aaeb1c67
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-03-19 16:39:49 +0100

    first try at hacking refactoring

---
+++ plugins/src/org/eclipse/jdt/internal/corext/refactoring/scripting/MyExtractMethodRefactoringContribution.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2011 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.corext.refactoring.scripting;
+
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+
+import org.eclipse.ltk.core.refactoring.Refactoring;
+import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+
+import org.eclipse.jdt.core.refactoring.descriptors.JavaRefactoringDescriptor;
+import org.eclipse.jdt.core.refactoring.descriptors.MyExtractMethodDescriptor;
+import org.eclipse.jdt.internal.corext.refactoring.JavaRefactoringArguments;
+import org.eclipse.jdt.internal.corext.refactoring.code.MyExtractMethodRefactoring;
+
+/**
+ * Refactoring contribution for the extract method refactoring.
+ *
+ * @since 3.2
+ */
+public final class MyExtractMethodRefactoringContribution extends JavaUIRefactoringContribution {
+
+	@Override
+	public Refactoring createRefactoring(JavaRefactoringDescriptor descriptor, RefactoringStatus status) throws CoreException {
+		JavaRefactoringArguments arguments= new JavaRefactoringArguments(descriptor.getProject(), retrieveArgumentMap(descriptor));
+		return new MyExtractMethodRefactoring(arguments, status);
+	}
+
+	@Override
+	public RefactoringDescriptor createDescriptor() {
+		return new MyExtractMethodDescriptor();
+	}
+
+	@Override
+	public RefactoringDescriptor createDescriptor(String id, String project, String description, String comment, Map<String, String> arguments, int flags) {
+		return new MyExtractMethodDescriptor(project, description, comment, arguments, flags);
+	}
+}
---
+++ plugins/src/refactoring/strategy/RefactoringContribution.java
@@ -0,0 +1,23 @@
+package refactoring.strategy;
+
+import java.util.Map;
+
+import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
+
+public class RefactoringContribution extends org.eclipse.ltk.core.refactoring.RefactoringContribution {
+
+	public RefactoringContribution() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public RefactoringDescriptor createDescriptor(String id, String project, String description, String comment,
+			Map<String, String> arguments, int flags) throws IllegalArgumentException {
+		
+		
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
