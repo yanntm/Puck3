61eb563d4ff1c683ec36dc957d00b2e41e1b7659
cedric <cedric@puck>
2021-09-16 14:40:08 +0200

    add auto refresh

--- plugins/fr.lip6.puck.parent/fr.lip6.puck.core/src/fr/lip6/puck/core/ui/ExtractGraph.java
+++ plugins/fr.lip6.puck.parent/fr.lip6.puck.core/src/fr/lip6/puck/core/ui/ExtractGraph.java
@@ -2,8 +2,13 @@ package fr.lip6.puck.core.ui;
 
 import java.io.IOException;
 import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.dom.CompilationUnit;
@@ -29,24 +34,26 @@ public class ExtractGraph extends AbstractCleanUp implements ICleanUp {
 			IProgressMonitor monitor) throws CoreException {
 
 		List<CompilationUnit> parsedCu = JavaParserHelper.parseSources(project, compilationUnits,monitor);
-		
+
 		// first traversal : grab packages, types, method declarations, attribute declarations
-		
+
 		PuckGraph graph = GraphBuilder.collectGraph(parsedCu);
-		
+
 		// let's have a look at it !
-		//System.out.println(graph);		
-		
+		//System.out.println(graph);
+
 		PuckInterpreter.findAndParsePuckFiles(project, graph);
-		
+
 		// let's build a graphviz file for it
 		try {
 			graph.exportDot(project.getProject().getLocation().toFile().getCanonicalPath() + "/graph.dot");
+			//refresh project
+			project.getProject().refreshLocal(IResource.DEPTH_ONE, new NullProgressMonitor());	
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		
+
 		return new RefactoringStatus();
 	}
 
